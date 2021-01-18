dc470e4738beb98560882d49ce04741395153803
CodingPapi <codingpapi@gmail.com>
2021-01-18 13:49:20 +0800

    update

--- stackAndQueue/stackAndQueue.go
+++ stackAndQueue/stackAndQueue.go
@@ -2,10 +2,11 @@ package stackAndQueue
 
 import (
 	"math"
+	"strconv"
 	// . "github.com/CodingPapi/LeetCodeGo/util"
 )
 
-// 155
+//155
 type MinStack struct {
 	diffStack []int
 	min       int
@@ -49,4 +50,143 @@ func (this *MinStack) GetMin() int {
 	return this.min
 }
 
-// 155 end
+//155 end
+
+//150
+func EvalRPN(tokens []string) int {
+	if len(tokens) < 3 {
+		return 0
+	}
+	stack := make([]int, 0)
+	for _, value := range tokens {
+		switch value {
+		case "+":
+			{
+				opLast := stack[len(stack)-1]
+				opFormer := stack[len(stack)-2]
+				stack = stack[:len(stack)-2]
+				stack = append(stack, opFormer+opLast)
+			}
+		case "-":
+			{
+				opLast := stack[len(stack)-1]
+				opFormer := stack[len(stack)-2]
+				stack = stack[:len(stack)-2]
+				stack = append(stack, opFormer-opLast)
+			}
+		case "*":
+			{
+				opLast := stack[len(stack)-1]
+				opFormer := stack[len(stack)-2]
+				stack = stack[:len(stack)-2]
+				stack = append(stack, opFormer*opLast)
+			}
+		case "/":
+			{
+				opLast := stack[len(stack)-1]
+				opFormer := stack[len(stack)-2]
+				stack = stack[:len(stack)-2]
+				stack = append(stack, opFormer/opLast)
+			}
+		default:
+			{
+				oper, _ := strconv.Atoi(value)
+				stack = append(stack, oper)
+			}
+		}
+	}
+
+	return stack[len(stack)-1]
+}
+
+//394
+type IntStack struct {
+	value []int
+}
+type StringStack struct {
+	value []string
+}
+
+func (this *IntStack) Push(x int) {
+	this.value = append(this.value, x)
+}
+
+func (this *IntStack) Pop() int {
+	top := this.value[len(this.value)-1]
+	this.value = this.value[:len(this.value)-1]
+	return top
+}
+
+func (this *IntStack) Top() int {
+	top := this.value[len(this.value)-1]
+	return top
+}
+
+func (this *StringStack) Push(x string) {
+	this.value = append(this.value, x)
+}
+
+func (this *StringStack) Pop() string {
+	top := this.value[len(this.value)-1]
+	this.value = this.value[:len(this.value)-1]
+	return top
+}
+
+func (this *StringStack) Top() string {
+	top := this.value[len(this.value)-1]
+	return top
+}
+
+func DecodeString(s string) string {
+	kStack := &IntStack{value: []int{}}
+	sStack := &StringStack{value: []string{""}}
+	kValue := ""
+	for _, value := range s {
+		switch {
+		case value == '[':
+			// "["
+			{
+				repeatK, _ := strconv.Atoi(kValue)
+				kStack.Push(repeatK)
+				kValue = ""
+				sStack.Push("")
+			}
+		case value == ']':
+			// "]"
+			{
+				latest := sStack.Pop()
+				repeatK := kStack.Pop()
+
+				result := ""
+				for i := 0; i < repeatK; i++ {
+					result += latest
+				}
+				latest = sStack.Pop()
+				result = latest + result
+				sStack.Push(result)
+			}
+		case value >= '0' && value <= '9':
+			// number
+			{
+				kValue += string(value)
+			}
+		default:
+			// char
+			{
+				latest := sStack.Pop()
+				latest += string(value)
+				sStack.Push(latest)
+			}
+		}
+
+	}
+
+	result := ""
+	for len(sStack.value) > 0 {
+		top := sStack.Pop()
+		if len(top) > 0 {
+			result = top + result
+		}
+	}
+	return result
+}
--- stackAndQueue/stackAndQueue_test.go
+++ stackAndQueue/stackAndQueue_test.go
@@ -2,7 +2,6 @@ package stackAndQueue
 
 import (
 	"testing"
-	// . "github.com/CodingPapi/LeetCodeGo/util"
 )
 
 func Test_RemoveDuplicateFromSortedList(t *testing.T) {
@@ -50,6 +49,128 @@ func Test_RemoveDuplicateFromSortedList(t *testing.T) {
 			// if got := RemoveDuplicateFromSortedList(l).ToString(); got != tt.want {
 			// 	t.Errorf("ListNode.toString() = %v, want %v", got, tt.want)
 			// }
+		})
+	}
+}
+
+func TestEvalRPN(t *testing.T) {
+	type args struct {
+		tokens []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				tokens: []string{"1", "2", "+"},
+			},
+			want: 3,
+		},
+		{
+			name: "test2",
+			args: args{
+				tokens: []string{"1", "2", "3", "*", "+"},
+			},
+			want: 7,
+		},
+		{
+			name: "test3",
+			args: args{
+				tokens: []string{"1", "4", "3", "/", "+"},
+			},
+			want: 2,
+		},
+		{
+			name: "test3",
+			args: args{
+				tokens: []string{"4", "13", "5", "/", "+"},
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := EvalRPN(tt.args.tokens); got != tt.want {
+				t.Errorf("EvalRPN() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDecodeString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test1",
+			args: args{
+				s: "3[a]2[bc]",
+			},
+			want: "aaabcbc",
+		},
+		{
+			name: "test2",
+			args: args{
+				s: "3[a2[c]]",
+			},
+			want: "accaccacc",
+		},
+		{
+			name: "test3",
+			args: args{
+				s: "2[abc]3[cd]ef",
+			},
+			want: "abcabccdcdcdef",
+		},
+		{
+			name: "test3",
+			args: args{
+				s: "abc3[cd]xyz",
+			},
+			want: "abccdcdcdxyz",
+		},
+		{
+			name: "test4",
+			args: args{
+				s: "ab2[c3[m]x2[d]y]z",
+			},
+			want: "abcmmmxddycmmmxddyz",
+		},
+		{
+			name: "test5",
+			args: args{
+				s: "10[a]2[bc]",
+			},
+			want: "aaaaaaaaaabcbc",
+		},
+		{
+			name: "test6",
+			args: args{
+				s: "2[a]2[2[b]c]",
+			},
+			want: "aabbcbbc",
+		},
+		{
+			name: "test7",
+			args: args{
+				s: "3[z]2[2[y]pq4[2[jk]e1[f]]]ef",
+			},
+			want: "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DecodeString(tt.args.s); got != tt.want {
+				t.Errorf("DecodeString() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
