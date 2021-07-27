386626bb220b694df4e05ca397b8760b58c87ab4
CodingPapi <codingpapi@gmail.com>
2021-07-27 21:40:43 +0800

    Add new

---
+++ randomPick/threeSum.go
@@ -0,0 +1,44 @@
+package randomSelect
+
+import (
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	result := make([][]int, 0)
+	for index, first := range nums {
+		if first > 0 {
+			break
+		}
+		if index > 0 && nums[index-1] == nums[index] {
+			continue
+		}
+		neededSum := -1 * first
+		for j := 1; j < len(nums)-index-1; j++ {
+			if j > 1 && nums[index+j-1] == nums[index+j] {
+				continue
+			}
+			k := len(nums) - j
+			tempSum := 0
+			for k > index+j {
+				if j > 1 && nums[index+j-1] == nums[index+j] {
+					j++
+					continue
+				}
+				tempSum = nums[index+j] + nums[k]
+				if tempSum > neededSum {
+					k--
+					continue
+				} else if tempSum < neededSum {
+					j++
+					continue
+				}
+				result = append(result, []int{first, nums[index+j], nums[k]})
+				j++
+			}
+			break
+		}
+	}
+	return result
+}
---
+++ randomPick/threeSum_test.go
@@ -0,0 +1,35 @@
+package randomSelect
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// {
+		// 	name: "first test",
+		// 	args: args{nums: []int{-1, 0, 1, 2, -1, -4}},
+		// 	want: [][]int{{-1, -1, 2}, {-1, 0, 1}},
+		// },
+		{
+			name: "second test",
+			args: args{nums: []int{0, 0, 0, 0}},
+			want: [][]int{{0, 0, 0}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
